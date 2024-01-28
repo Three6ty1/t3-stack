2288214dfdcd45fdc12019b87df30e1377a4fb12
Three6ty1 <wk.zhang02@gmail.com>
2024-01-28 20:04:58 +1100

    Added backend and frontend helper file for enums and types

---
+++ src/helper/helper.ts
@@ -0,0 +1,106 @@
+export function randomInteger(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export const ICON_DIR = 'https://raw.githubusercontent.com/Three6ty1/ak-wordle-icons/main/avatars/'
+export const PROFESSION_ICON_DIR = 'https://raw.githubusercontent.com/Three6ty1/ak-wordle-icons/main/classes/black/'
+export type GuessType = [string, string, string, string, number];
+
+export enum GuessTypeValue {
+    name = 0,
+    charId = 1,
+    class = 2,
+    archetype = 3,
+    rarity = 4,
+}
+
+export enum Range {
+    Lower = "Lower",
+    Correct = "Correct",
+    Higher = "Higher",
+}
+
+export enum Correctness {
+    Wrong = "Wrong",
+    Half = "Half",
+    Correct = "Correct",
+}
+
+export const wordleColors = {
+    incorrect: '#919090',
+    higher: '#08aadd',
+    lower: '#c80000',
+    half: '#FFAC1C',
+    correct: '#6BBF59',
+}
+
+export function getOperatorIconUrl(charId: string, rarity: number) {
+    let url;
+    if (rarity > 3) {
+        url = ICON_DIR + charId + '_2.png'
+    } else {
+        url = ICON_DIR + charId + '.png'
+    }
+
+    return url;
+}
+
+export function getProfessionIconUrl(profession: string) {
+    return PROFESSION_ICON_DIR + 'icon_profession_' + profession.toLowerCase() + '_large.png';
+}
+
+export const raceToolTips = {
+    'Ægir': 'Seaborn and Aquatic animals',
+    'Anasa': 'Asuras, Rakshasas and Yakshas (Hindu-Buddhist mythology)',
+    'Anaty': 'Otters, Weasels, Raccoons and Wolverines',
+    'Anura': 'Frogs',
+    'Archosauria': 'Crocodilians',
+    'Aslan': 'Lions',
+    'Caprinae': 'Goats and Sheeps',
+    'Cautus': 'Rabbits and Hares',
+    'Cerato': 'Rhinoceroses',
+    'Chimera': 'Hybrid race',
+    'Draco': 'European dragons',
+    'Durin': 'Dwarves',
+    'Elafia': 'Deers',
+    'Feline': 'Cats, Big cats and Mongooses (Exc. Aslan: Lions)',
+    'Forte': 'Bovines and camelids',
+    'Itra': 'Musk Deer',
+    'Kuranta': 'Horses and Zebras',
+    'Kylin': 'Qilin (Chinese mythology)',
+    'Liberi': 'Birds and Bird-like mythological creatures',
+    'Lung': 'Chinese dragons',
+    'Lupo': 'Wolves',
+    'Manticore': 'Manticore (one) (Persian mythology)',
+    'Oni': 'Oni (Japanese mythology)',
+    'Perro': 'Dogs',
+    'Petram': 'Turtles and Tortoises',
+    'Pilosa': 'Sloths',
+    'Pythia': 'Serpents',
+    'Rebbah': 'Hyenas',
+    'Robot': 'Beep Boop',
+    'Sankta': 'Angels (Abrahamic religions)',
+    'Sarkaz': 'Demons (Abrahamic religions)',
+    'Savra': 'Lizards and Salamanders',
+    'Ursus': 'Bears',
+    'Vouivre': 'Wyverns, Wyrms, Hydras, Amphipteres (Winged Serpents)\nand Drakes',
+    'Vulpo': 'Foxes',
+    'Zalak': 'Rodents, Opposums and Moles',
+    'Credits to': 'Arknights Fandom wiki'
+}
+
+export const guessCategoryToolTips = {
+    'Operator': null,
+    'Gender': 'Male/Female',
+    'Race': null,
+    'Allegiance': 'The Group/Team > Nation the operator represents.',
+    'Infected': 'Yes/No/Undisclosed',
+    'Profession': null,
+    'Rarity': '1/2/3/4/5/6 star',
+    'Cost (E2)': 'Cost is compared at E2 Pot0.'
+}
+
+export const costToolTips = {
+    'Higher': 'The E2 cost of the correct operator is HIGHER\nthan this operators E2 cost.',
+    'Lower': 'The E2 cost of the correct operator is LOWER\nthan this operators E2 cost.'
+}
