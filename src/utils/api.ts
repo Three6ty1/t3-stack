ad072bdea4d38276c5bac00ad255028762cbbf3b
Three6ty1 <wk.zhang02@gmail.com>
2024-01-31 00:24:05 +1100

    Enabled SSR (?)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -19,7 +19,8 @@ const getBaseUrl = () => {
 
 /** A set of type-safe react-query hooks for your tRPC API. */
 export const api = createTRPCNext<AppRouter>({
-  config() {
+  config(opts) {
+    const { ctx } = opts;
     return {
       /**
        * Transformer used for data de-serialization from the server.
@@ -35,12 +36,23 @@ export const api = createTRPCNext<AppRouter>({
        */
       links: [
         loggerLink({
-          enabled: (opts) =>
+          enabled: (opts) => false
+            /** 
             process.env.NODE_ENV === "development" ||
-            (opts.direction === "down" && opts.result instanceof Error),
+            (opts.direction === "down" && opts.result instanceof Error),*/
         }),
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
+          headers() {
+            if (!ctx?.req?.headers) {
+              return {};
+            }
+            // To use SSR properly, you need to forward client headers to the server
+            // This is so you can pass through things like cookies when we're server-side rendering
+            return {
+              cookie: ctx.req.headers.cookie,
+            };
+          },
         }),
       ],
     };
@@ -50,7 +62,7 @@ export const api = createTRPCNext<AppRouter>({
    *
    * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
    */
-  ssr: false,
+  ssr: true,
 });
 
 /**
