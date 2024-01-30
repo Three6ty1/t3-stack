1139cd9363fb75579ddc3e90da828fe78ecf04bb
Three6ty1 <wk.zhang02@gmail.com>
2024-01-31 00:21:23 +1100

    Added routes for compare and allNames proceduces

--- src/server/api/routers/wordle.ts
+++ src/server/api/routers/wordle.ts
@@ -1,44 +1,28 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import * as wordle from "./wordleServer";
-
+import { GuessResult } from "./wordleServer";
 export const wordleRouter = createTRPCRouter({
     // Mutation because we might generate a new operator
     stats: publicProcedure
-        .mutation(async ({ ctx }) => {
-            return await wordle.getOperatorStats(ctx.db);
+        .query(async ({ ctx }) => {
+            const res = await wordle.getOperatorStats(ctx.db);
+            return res;
     }),
     
     compare: publicProcedure
-        .input(z.object({ guess: z.string() }))
+        .input(z.object(
+            { 
+                guess: z.string(), 
+                guesses: z.array(z.string())
+            }
+        ))
         .query(async ({ ctx, input }) => {
-            return await wordle.compareGuess(ctx.db, input.guess);
+            return await wordle.compareGuess(ctx.db, input.guess, input.guesses);
     }),
 
     allNames: publicProcedure
         .query(async ({ ctx }) => {
-            return await wordle.getAllOperatorNames(ctx.db);
+            return await wordle.getAllOperators(ctx.db);
         }),
-    
-
-    /**
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      // simulate a slow db call
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
-  getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-  }),
-  */
 });
