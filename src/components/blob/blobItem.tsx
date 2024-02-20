493f64bc90c01580bc780f7545a9e2c65692c026
Three6ty1 <wk.zhang02@gmail.com>
2024-02-20 15:33:57 +1100

    variable sizes fixed

--- src/components/blob/blobItem.tsx
+++ src/components/blob/blobItem.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
+const defSize = 100
+const defMargin = 4
+
 export default function BlobItem(props: any) {
   const {
     attributes,
@@ -18,21 +21,25 @@ export default function BlobItem(props: any) {
     transition,
     zIndex: isDragging ? "100" : "auto",
     opacity: isDragging ? 0.3 : 1,
-    width: props.id != "8" ? "100px" : "208px",
-    height: props.id != "8" ? "100px" : "208px",
-    gridRowStart: props.id === "8" ? "span 2" : undefined,
-    gridColumnStart: props.id === "8" ? "span 2" : undefined,
+    width: props.name != 3 ? defSize : defSize * 2 + defMargin * 2,
+    height: props.name != 3 ? defSize : defSize * 2 + defMargin * 2,
+    margin: defMargin,
+    gridRowStart: props.name === 3 ? "span 2" : undefined,
+    gridColumnStart: props.name === 3 ? "span 2" : undefined,
   };
-
   return (
-    <div
-      ref={setNodeRef}
-      style={style}
-      className="m-1 border-2 border-solid border-red-500 bg-gray-500"
-      {...listeners}
-      {...attributes}
-    >
-      {props.value}
-    </div>
+    <div ref={setNodeRef} style={style} className="border-2 border-solid border-red-500 bg-gray-500 rounded-md p-1 text-sm">
+      <button {...listeners} {...attributes}>
+        Drag handle
+      </button>
+      <div
+        style={{
+          border: "1px solid black",
+          borderColor: "black"
+        }}
+      >
+        {props.value}
+      </div>
+  </div>
   );
 }
