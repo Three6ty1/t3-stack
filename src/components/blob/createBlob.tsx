d66ebb19bb295e8279f0ed632a14c0a3e7f0cbc3
Three6ty1 <wk.zhang02@gmail.com>
2024-02-21 00:33:52 +1100

    Basic create blob finished

--- src/components/blob/createBlob.tsx
+++ src/components/blob/createBlob.tsx
@@ -1,11 +1,70 @@
+import { BlobTags } from "@prisma/client";
+import { useState } from "react";
+
+const defaultBlob = {
+  title: "",
+  description: "",
+  tags: [] as BlobTags[],
+  images: [] as string[],
+  videos: [] as string[],
+}
+
 export default function CreateBlob() {
+  const [blob, setBlob] = useState(defaultBlob);
+  console.log(blob)
   return (
     <>
-      <button className="btn" onClick={()=>(document.getElementById('create_blob_modal')! as HTMLDialogElement).showModal()}>open modal</button>
+      <button
+        className="btn"
+        onClick={() =>
+          (
+            document.getElementById("create_blob_modal")! as HTMLDialogElement
+          ).showModal()
+        }
+      >
+        open modal
+      </button>
       <dialog id="create_blob_modal" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          
+        <div className="modal-box space-y-4 flex flex-col overflow-visible">
+          <h3 className="text-lg font-bold">Hello!</h3>
+          <input
+            type="text"
+            placeholder="Enter a title"
+            className="input input-bordered w-full max-w-xs"
+            value={blob.title}
+            onChange={(value) => setBlob({...blob, title: value.target.value})}
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+            value={blob.description}
+            onChange={(value) => setBlob({...blob, description: value.target.value})}
+          />
+          <details className="dropdown">
+            <summary className="m-1 btn">open or close</summary>
+            <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+              {Object.values(BlobTags).map((tag) => (
+                <li>
+                  <div className="form-control">
+                    <label className="label cursor-pointer">
+                      <span className="label-text">{tag}</span> 
+                      <input type="checkbox" className="checkbox" />
+                    </label>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </details>
+          <input
+            type="file"
+            className="file-input file-input-bordered w-full max-w-xs"
+          />
+          <input
+            type="text"
+            placeholder="Type here"
+            className="input input-bordered w-full max-w-xs"
+          />
           {/* 
             title: input.title,
             description: input.description,
@@ -19,5 +78,5 @@ export default function CreateBlob() {
         </form>
       </dialog>
     </>
-  )
+  );
 }
